6523ac97e59f387bc725af06a02705f65bdc63c6
vrapolinario <viniap@microsoft.com>
2020-03-02 08:47:51 -0800

    Update Storage Management.ps1

--- Containers For ITPros/Storage Management.ps1
+++ Containers For ITPros/Storage Management.ps1
@@ -1,2 +1,7 @@
 #Run container with mounted volume
 docker run -d --name testcontainer -p 8081:80 -v C:\Test:C:\ContainerTest mcr.microsoft.com/windows/servercore/iis:windowsservercore-ltsc2019
+
+#Run container with SMB mount
+$creds = Get-Credential
+New-SmbGlobalMapping -RemotePath \\contosofileserver\share1 -Credential $creds -LocalPath G:
+docker run -d --name testcontainer -p 8081:80 -v G:\ContainerData:C:\ContainerTest mcr.microsoft.com/windows/servercore/iis:windowsservercore-ltsc2019
