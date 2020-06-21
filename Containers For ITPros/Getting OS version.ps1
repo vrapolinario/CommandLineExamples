62b8b8e17dc71591bc722af1c0d7d90b98430985
vrapolinario <viniap@microsoft.com>
2020-06-21 14:51:50 -0700

    Update Getting OS version.ps1

--- Containers For ITPros/Getting OS version.ps1
+++ Containers For ITPros/Getting OS version.ps1
@@ -1,3 +1,4 @@
 [System.Environment]::OSVersion.Version
 (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").ReleaseId
 Get-WmiObject -Class Win32_OperatingSystem | ForEach-Object -MemberName Caption
+#systeminfo /fo csv | ConvertFrom-Csv | select OS*, System*, Hotfix* | Format-List
